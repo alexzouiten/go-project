f1b919ffe3ca3dd33e711670a82f1f57180f4ada
Alexandre Zouiten <alexandre.zouiten1@gmail.com>
2026-09-15 13:02:54 +0200

    fix wrong go file name

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -euxo pipefail
+set -euo pipefail
 
 if [ $# -ne 2 ]; then
     echo "usage: ./setup.sh <owner-name> <repo-name>"
@@ -22,7 +22,7 @@ for file in $(grep -rl @@OWNER@@ --exclude-dir .git); do
     sed -i "s/@@OWNER@@/$OWNER/g" "$file"
 done
 
-mv cmd/main/main.go "cmd/main/$REPO_NAME"
+mv cmd/main/main.go "cmd/main/$REPO_NAME.go"
 mv cmd/main "cmd/$REPO_NAME"
 
 rm setup.sh
